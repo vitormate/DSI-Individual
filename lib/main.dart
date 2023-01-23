31616e36e938818026463c73248f93cecc593667
Vitor Mateus <vmateusaguiar@gmail.com>
2023-01-23 12:18:28 -0300

    feat: topico 2 da atividade individual finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -60,27 +60,14 @@ class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+  String view = 'lista';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // NEW from here ...
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions',
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        // to here.
+  corpo() {
+    if (view == 'lista') {
+      return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return const Divider();
-
           final index = i ~/ 2;
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
@@ -109,6 +96,82 @@ class _RandomWordsState extends State<RandomWords> {
             },
           );
         },
+      );
+    } else {
+      return GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          final alreadySaved = _saved.contains(_suggestions[i]);
+          return Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  _suggestions[i].asPascalCase,
+                  style: _biggerFont,
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      if (alreadySaved) {
+                        _saved.remove(_suggestions[i]);
+                      } else {
+                        _saved.add(_suggestions[i]);
+                      }
+                    });
+                  },
+                  icon: Icon(
+                    alreadySaved ? Icons.favorite : Icons.favorite_border,
+                    color: alreadySaved ? Colors.red : null,
+                    semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    view = 'lista';
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.list_alt_outlined),
+                ),
+                IconButton(
+                  onPressed: () {
+                    view = 'bloco';
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.dataset_outlined),
+                ),
+              ],
+            ),
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            ),
+          ],
+        ),
+        body: corpo(),
       ),
     );
   }
